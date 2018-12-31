931f183cc69b67dc9571781b36479b6e251afa52
mdhvkothari <mdhvkothari007@gmail.com>
2018-12-31 14:34:44 +0530

    commit

--- data_structure/double_linked_list.c
+++ data_structure/double_linked_list.c
@@ -12,12 +12,20 @@ struct node* root = NULL ;
 
 void append(int);
 void print();
+void addfirst(int);
+void length();
 
 void main(){
 	append(10);
 	print();
+	length();
 	append(20);
 	print();
+	length();
+	addfirst(50);
+	print();
+	length();
+
 
 }
 
@@ -66,4 +74,43 @@ void print(){
 		temp = temp->right;
 	}
 	printf("\n");
+}
+
+
+void addfirst(int data){
+
+	struct node* temp;
+	
+	temp = (struct node*)malloc(sizeof(struct node));
+	
+	temp->data = data;
+	temp->left = NULL;
+	temp->right = NULL;
+
+	//if there is no node present in the list then
+
+	if(root == NULL){
+		root  = temp;
+	}
+	//when some nodes are already present
+	else{
+		//first we have to connect right site then left
+
+		temp->right = root;
+		root->left = temp;
+
+		root = temp;
+
+	}
+}
+
+void length(){
+	struct node* temp;
+	temp = root;
+	int count=0;
+	while(temp!=NULL){
+		count++;
+		temp = temp->right;
+	}
+	printf("%d\n",count );
 }
