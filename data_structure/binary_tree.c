2b9832bc8b320b887299cf6a1364989349aee73d
= <mdhvkothari007@gmail.com>
2019-01-10 22:41:43 +0530

    commit

---
+++ data_structure/binary_tree.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+  int data;
+  struct node* left;
+  struct node* right;
+};
+
+struct node* root = NULL;
+
+
+void insert(int);
+
+void main(){
+  insert(50);
+
+}
+
+void insert(int data){
+
+  struct node  *t,*parent ;
+  t = (struct node*)malloc(sizeof(struct node));
+  t->data = data;
+  t->left = NULL;
+  t->right = NULL;
+
+  parent= root;
+  if(root == NULL){
+    root = t;
+  }
+
+  else{
+    struct node* current;
+    current = root;
+    while (current){
+      parent = current;
+      if (t->data > current->data){
+        current = current->right;
+      }
+      else{
+        current = current->left;
+      }
+    }
+    if (t->data > parent->data){
+        parent->right = t;
+    }
+    else{
+      parent->left = t;
+    }
+  }
+}
