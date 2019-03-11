b7c4c5694fc98f991feeae47ced79a7c11fd4626
= <mdhvkothari007@gmail.com>
2019-03-11 20:36:44 +0530

    commit

--- data_structure/circular_queue.c
+++ data_structure/circular_queue.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#CAPACITY 5
+#define CAPACITY 5
 void insert(int);
 void delete();
 void tranverse();
@@ -10,12 +10,14 @@ int data;
 void main(){
   insert(10);
   insert(20);
+  delete();
+  tranverse();
 
 }
 
 
 void insert(int element){
-  if(front == rear+1  || rear = CAPACITY-1 ){
+  if(front == rear+1  || rear == CAPACITY-1 ){
 
   }
   else if(front==-1&&rear==-1){
@@ -33,22 +35,37 @@ void insert(int element){
     rear ++;
     cqueue[rear] = element;
   }
+  printf("Sucessfully insert\n" );
 }
 
 
 void delete(){
-  if(front==-1&& rear == -1){
+  if(front==-1 && rear == -1){
     printf("Queue is empty\n");
   }
-  else if(fornt==rear){
+  else if(front==rear){
     front=rear=-1;
   }
-  else if(fornt==CAPACITY-1){
+  else if(front==CAPACITY-1){
     data = cqueue[front];
     front =0;
   }
   else{
     data  = cqueue[front];
     front++;
+  }
+  printf("%d Sucessfully deleted\n",data );
+}
+
+// not working
+void tranverse(){
+  if(front==-1){
+    printf("Queue is empty\n");
+  }
+
+  else if(front==0){
+      for (int i=0;i<CAPACITY;i++){
+        printf("%d\n",cqueue[i] );
+      }
   }
 }
